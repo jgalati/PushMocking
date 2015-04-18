473f3388defde9a5ef355080c68a280f1dad9cf6
Julián Galati <egjuli@gmail.com>
2015-04-18 13:42:53 -0300

    Registred fixed

--- www/PushMockingPlugin.js
+++ www/PushMockingPlugin.js
@@ -1,20 +1,8 @@
-var PushMocking = function() {
-};
+var PushMocking = {
+    
+showPush : function(successCallback, errorCallback, title, message, appName) {
 
-PushMocking.prototype.showPush = function(successCallback, errorCallback, title, message, appName) {
-    if (errorCallback == null) { errorCallback = function() {}}
-
-    if (typeof errorCallback != "function")  {
-        console.log("PushMocking.showPush failure: failure parameter not a function");
-        return
-    }
-
-    if (typeof successCallback != "function") {
-        console.log("PushMocking.showPush failure: success callback parameter must be a function");
-        return
-    }
-
-    cordova.exec(
+        cordova.exec(
             successCallback, 
             errorCallback, 
             "PushMockingPlugin", 
@@ -24,18 +12,11 @@ PushMocking.prototype.showPush = function(successCallback, errorCallback, title,
                 "messae": message,
                 "appName": appName
             }]
-    );
+        );
+    }
 };
 
 //-------------------------------------------------------------------
 
-if(!window.plugins) {
-    window.plugins = {};
-}
-if (!window.plugins.pushMocking) {
-    window.plugins.pushMocking = new PushMocking();
-}
 
-if (typeof module != 'undefined' && module.exports) {
-  module.exports = PushMocking;
-}
+module.exports = PushMocking;
